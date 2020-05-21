1a44a775e962aea97aacb6ec57bad1bc9a581986
NickKantser <kolia.falka@gmail.com>
2020-05-21 17:19:17 +0200

    Update sSg.js

--- scripts/sSg.js
+++ scripts/sSg.js
@@ -1,5 +1,5 @@
 var GEN = ["města", "moře", "náměstí"];
 var DAT = ["městu", "moři", "náměstí"];
 var ACC = ["město", "moře", "náměstí"];
-var LOK = ["městu", "moři", "náměstí"];
+var LOK = [["městu", "městě"], "moři", "náměstí"];
 var INSTR = ["městem", "mořem", "náměstím"];
