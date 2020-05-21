93f52bae83f44045fa559f8210cf00edafd09e63
NickKantser <kolia.falka@gmail.com>
2020-05-21 16:16:59 +0200

    Add files via upload

---
+++ scripts/ideaPl.js
@@ -0,0 +1,5 @@
+var GEN = ["idejí", "tracheí"];
+var DAT = [["ideám", "idejím"], ["tracheám", "tracheím"]];
+var ACC = [["idey", "ideje"], "trachey"];
+var LOK = [["ideách", "idejích"], ["tracheách", "tracheích"]];
+var INSTR = [["ideami", "idejemi"], "tracheami"];
---
+++ scripts/ideaSg.js
@@ -0,0 +1,5 @@
+var GEN = [["idey", "ideje"], "trachey"];
+var DAT = ["ideji", "trachei"];
+var ACC = ["ideu", "tracheu"];
+var LOK = ["ideji", "trachei"];
+var INSTR = [["ideou", "idejí"], "tracheou"];
