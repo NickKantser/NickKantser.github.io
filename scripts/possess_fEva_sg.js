2569daaa8c8ab82d62c85b916c2ae778e9b21c55
NickKantser <kolia.falka@gmail.com>
2020-05-29 02:21:16 +0200

    Update possess_fEva_sg.js

--- scripts/possess_fEva_sg.js
+++ scripts/possess_fEva_sg.js
@@ -1,5 +1,5 @@
 var GEN = ["evina", "evina", "eviny", "evina"];
-var DAT = ["evinu", "evinu", "evině", "evině"];
+var DAT = ["evinu", "evinu", "evině", "evinu"];
 var ACC = ["evina", "evin", "evinu", "evino"];
 var LOK = ["evině", "evině", "evině", "evině"];
 var INSTR = ["eviným", "eviným", "evinou", "eviným"];
