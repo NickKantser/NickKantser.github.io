549c80d6070d10e7ce803590d0ebcbd74632f923
NickKantser <kolia.falka@gmail.com>
2020-06-22 00:27:25 +0200

    Update sPl.js

--- scripts/sPl.js
+++ scripts/sPl.js
@@ -1,5 +1,5 @@
 var GEN = ["měst", "moří", "náměstí"];
 var DAT = ["městům", "mořím", "náměstím"];
 var ACC = ["města", "moře", "náměstí"];
-var LOK = ["městech", "mořech", "náměstích"];
+var LOK = ["městech", "mořích", "náměstích"];
 var INSTR = ["městy", "moři", "náměstími"];
